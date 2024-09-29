595292f19e9e9b8e775e3ef9cadf4a29b2a30ec5
haquanghuy <phandat20ew@gmail.com>
2024-09-30 01:55:59 +0700

    testimonial

--- js/main.js
+++ js/main.js
@@ -2,8 +2,85 @@ const NavTriggerBtn = document.querySelector('#nav_trigger_btn');
 const NavMenu = document.querySelector('#nav_menu');
 
 //event listener
-NavTriggerBtn.addEventListener('click' , () =>{
+NavTriggerBtn.addEventListener('click', () => {
     NavMenu.classList.toggle('nav-is-open');
 
 }
 );
+// swiper 
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    //breakpoints
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints:{
+        320:{
+            slidesPerView:1,
+        },
+        960:{
+            slidesPerView:2,
+        },
+        1200:{
+            slidesPerView:3,
+        },
+    }
+});
+
+//animations
+const sr = ScrollReveal({
+    origin:'bottom',
+    distence:'60px',
+    duration:3000,
+    delay:600,
+    //reset:true,
+});
+//hero
+sr.reveal('.hero__text',{ origin:'top'});
+
+
+//step
+sr.reveal('.steps__step',{ distanse:'100px', Interval:100 });
+
+//about
+sr.reveal('.about__text',{ origin:'left'});
+sr.reveal('.about__img',{ origin:'right', delay:800 });
+
+
+//testimonial
+sr.reveal('.testimonial__bg',{ delay:800});
+sr.reveal('.testimonial__title');
+sr.reveal('.testimonial__slider',{ delay:1000});
+
+//brand
+sr.reveal('.brands__img',{ delay:600, distanse:'100px', interval:100 });
+
+
+//work
+sr.reveal('.work__title');
+sr.reveal('.work__subtitle',{ delay:800});
+sr.reveal('.work__grid',{ delay:1000});
+
+
+//stats
+sr.reveal('.stats');
+sr.reveal('.stats__item',{ distanse:'100px', interval:100 });
+
+
+//news
+sr.reveal('.news__title ');
+sr.reveal('.sub__title ',{ delay:800});
+
+sr.reveal('.news__item ',{ distanse:'100px', interval:100, delay:1000 });
+
+
+//contact
+sr.reveal('.contact__container');
+sr.reveal('.contact__text ',{ delay:800});
+
+//footer
+sr.reveal('.footer__item',{ distanse:'100px', interval:100, delay:1000 });
+sr.reveal('.footer__Alanotis');
